df8d100ce25b8a556174eceb7b54973e1ed43ba5
benve-meucci <56107086+benve-meucci@users.noreply.github.com>
2026-02-25 11:02:58 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,20 +1,25 @@
 <?php
 use Slim\Factory\AppFactory;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/controllers/AlunniController.php';
 
 $app = AppFactory::create();
 
+$app->get('/test', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Test page");
+    return $response;
+});
 
 $app->get('/hello/{name}', function (Request $request, Response $response, array $args) {
     $name = $args['name'];
     $response->getBody()->write("Hello, $name");
-
     return $response;
 });
 
 $app->get('/alunni', "AlunniController:index");
-
 
 $app->run();
